2fc4049e5687b05c8cf1047f732df9bf3d4ab570
PlateformeBH <bhplateforme@gmail.com>
2022-05-09 10:12:16 +0100

    first commit

---
+++ src/test/java/org/bh/estimationservice/EstimationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bh.estimationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstimationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
